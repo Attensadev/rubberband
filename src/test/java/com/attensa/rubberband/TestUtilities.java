33ce181a7dcf5fb1153555d68468cada393f778b
John Watson <jkwatson@gmail.com>
2016-01-05 13:50:13 -0800

    fix test that would break on an empty database

--- src/test/java/com/attensa/rubberband/TestUtilities.java
+++ src/test/java/com/attensa/rubberband/TestUtilities.java
@@ -1,5 +1,6 @@
 package com.attensa.rubberband;
 
+import com.attensa.rubberband.data.SearchRequest;
 import com.google.gson.Gson;
 import okhttp3.OkHttpClient;
 import org.junit.Ignore;
@@ -10,4 +11,15 @@ public class TestUtilities {
     public static RubberbandClient buildClient() {
         return new RubberbandClient(new OkHttpClient(), new Gson(), "http://localhost:9200");
     }
+
+    public static long waitForResultsToShowUp(RubberbandClient client, SearchRequest searchRequest) throws InterruptedException {
+        long count;
+        int tries = 0;
+        do {
+            count = client.count("animals", searchRequest);
+            Thread.sleep(200);
+        } while (count != 0 && tries++ < 10);
+        return count;
+    }
+
 }
