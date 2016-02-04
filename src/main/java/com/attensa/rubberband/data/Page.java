f61eabffec93d18cc4e1487b378daffaec3c35b1
John Watson <jkwatson@gmail.com>
2016-02-04 11:14:12 -0800

    add an alternate constructor for existing clients

--- src/main/java/com/attensa/rubberband/data/Page.java
+++ src/main/java/com/attensa/rubberband/data/Page.java
@@ -1,7 +1,7 @@
 package com.attensa.rubberband.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Value;
-import org.jooq.lambda.Seq;
 
 import java.util.List;
 import java.util.Map;
@@ -10,6 +10,7 @@ import java.util.function.Function;
 import static org.jooq.lambda.Seq.seq;
 
 @Value
+@AllArgsConstructor
 public class Page<T> {
     List<T> contents;
     PageRequest request;
@@ -17,6 +18,10 @@ public class Page<T> {
 
     //here until a better api can be figured out.
     Map<String, Object> aggregations;
+
+    public Page(List<T> contents, PageRequest request, long total) {
+        this(contents, request, total, null);
+    }
 
     public <S> Page<S> map(Function<T, S> conversion) {
         return new Page<>(seq(contents).map(conversion).toList(), request, total, aggregations);
